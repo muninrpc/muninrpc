1e2b7f224158ae4d1d7c37d0148fcbcbd706ca36
Ed <15ryane@gmail.com>
2019-03-25 16:02:12 -0700

    added successful packaging of grpc

--- app/src/components/Result.jsx
+++ app/src/components/Result.jsx
@@ -4,10 +4,10 @@ export default function Result() {
   return (
     <div id="result">
       <p>Request Status</p>
-      <div class="resultbox">
+      <div className="resultbox">
         Resulting response will display here
       </div>
-      <div class="resultmetrics">
+      <div className="resultmetrics">
         0.000000000042ms
       </div>
     </div>
