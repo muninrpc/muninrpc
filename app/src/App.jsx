65a2d56e97bd1ded9086aa546150753124a864cb
Ed <15ryane@gmail.com>
2019-03-27 10:29:03 -0700

    attempting merge

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,7 @@
 import React, {Component} from 'react'
-import Target from './components/Target.jsx';
-import Result from './components/Result.jsx';
-import ProtoPreview from './components/ProtoPreview.jsx';
-import MessageBody from './components/MessageBody.jsx';
-import ServiceConfig from './components/ServiceConfig.jsx';
-
+import Left from './components/Left.jsx';
+import Right from './components/Right.jsx';
+const grpc = require('grpc')
 export default class App extends Component {
 
   constructor() {
@@ -13,51 +10,15 @@ export default class App extends Component {
       inputBox: "",
       items: []
     }
-    this.handleDeleteButton = this.handleDeleteButton.bind(this)
-    this.handleInputBoxChange = this.handleInputBoxChange.bind(this)
-    this.submitItem = this.submitItem.bind(this)
-  }
 
-  handleDeleteButton(e) {
-    const idx = e.target.name
-    this.setState(prevState => (
-      {...prevState.items.splice(idx, 1)}
-    ))
-
-  }
-
-  handleInputBoxChange(e) {
-    this.setState({
-      inputBox: e.target.value
-    })
-  }
-
-  submitItem(){
-    let items = [...this.state.items, this.state.inputBox]
-    // items.push(this.state.inputBox)
-    this.setState({
-      inputBox: "",
-      items
-    })
-  }
-
-  startServer(){
-    const { fork } = require('child_process');
-    const ps = fork(`${__dirname}/server.js`);
   }
 
   render() {
     return (
-      <React.Fragment>
-        <h1>MuninRPC</h1>
-        <div className="app">
-          <Target />
-          <Result />
-          <ProtoPreview />
-          <MessageBody />
-          <ServiceConfig />
-        </div>
-      </React.Fragment>
+      <div className="app">
+        <Left />
+        <Right />
+      </div>
     )
 
   }
