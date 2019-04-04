8c22a1bce6b2a89cd562fdfa01dddf96b81ba93f
Ray Yao <rocaray51@gmail.com>
2019-04-04 15:12:09 -0700

    Server Response display

--- app/src/components/Right.tsx
+++ app/src/components/Right.tsx
@@ -8,11 +8,12 @@ export namespace RightProps {
 }
 
 export function Right(props: RightProps.Props, context?: any) {
+
   return (
     <div className="right-half">
       <h2>Server Response</h2>
-      <div className="response-display">Placeholder</div>
-      <div className="response-metrics">{props.responseMetrics}</div>
+      <pre className="response-display">{Object.keys(props.serverResponse).length ? JSON.stringify(props.serverResponse, null, 2) : ''}</pre>
+      <div className="response-metrics">{Object.keys(props.serverResponse).length ? 'Success' : ''}</div>
     </div>
   );
 }
